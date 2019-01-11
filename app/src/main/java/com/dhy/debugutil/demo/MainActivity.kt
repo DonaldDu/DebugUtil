5c357eab5afef443b866a535ad2c03c0c1058efa
Donald <onemise@qq.com>
2019-01-11 15:44:19 +0800

    fix demo error

--- app/src/main/java/com/dhy/debugutil/demo/MainActivity.kt
+++ app/src/main/java/com/dhy/debugutil/demo/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        api = ApiHolderUtil().createHolderApi(ApiHolder::class.java)
+        api = ApiHolderUtil(ApiHolder::class).api
 
         val user = TestUserUtil(this, api, null, "")
         user.initOnViewLongClick(buttonUser)
